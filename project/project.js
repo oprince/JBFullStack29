edd978fa2d7d33b8fa0d00e55434f65fbce3b4d9
adirhno <94399027+adirhno@users.noreply.github.com>
2021-12-02 19:22:20 +0200

    new update

---
+++ project/project.js
@@ -0,0 +1,55 @@
+function addTask(){
+
+    const task={
+ 
+         Name : this.Name,
+         dueDate : this.dueDate,
+         dueTime : this.dueTime
+     };
+ 
+ task.Name=document.getElementById('task').value
+ task.dueDate= document.getElementById('date').value
+ task.dueTime= document.getElementById('time').value;
+ 
+ 
+ 
+ 
+ let newTask= document.createElement("div");
+ 
+ let taskname= document.createElement("p");
+ let taskdate= document.createElement("p");
+ let tasktime= document.createElement("p");
+ 
+ newTask.appendChild(taskname);
+ newTask.appendChild(taskdate);
+ newTask.appendChild(tasktime);
+ 
+ 
+ let taskN=document.createTextNode(task.Name);
+ let taskD=document.createTextNode(task.dueDate);
+ let taskT=document.createTextNode(task.dueTime);
+ 
+ taskname.appendChild(taskN);
+ taskdate.appendChild(taskD);
+ tasktime.appendChild(taskT);
+ 
+ 
+ let br = document.createElement("br");
+ taskname.appendChild(br);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ document.getElementById('taskDiv').appendChild(newTask);
+ 
+ 
+ 
+ 
+ }
+ 
+ 
+ 
