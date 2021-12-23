c1d2b399b2de11605ac93cf58bc9d38a8e5ca205
orit <oprince10@gmail.com>
2021-12-23 19:30:40 +0200

    draw rectangles - solution

--- Ex/2021-12-20/js/draw_shapes.js
+++ Ex/2021-12-20/js/draw_shapes.js
@@ -15,8 +15,39 @@ function fillBackground(){
     grd.addColorStop(0, "orange");
     grd.addColorStop(1, "red");
     // Fill with gradient
-    ctx.fillStyle = grd;
+    ctx.fillStyle = "yellow";
     ctx.fillRect(0, 0, canvas.width, canvas.height);  
+}
+function drawRectangle(rect){
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = rect.color;
+    ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+}
+function drawRects(){
+    let colors = ["#184CE8", "#1FD1C5", "#A9B3D8", "#3C4874"];
+    let currRect = {
+        x: 150, 
+        y: 10,
+        width: 130,
+        height: 130,
+        color: colors[0]
+    }
+    let minimalRect = {
+        width: 10,
+        height: 10
+    }
+    let rectsCount = 3
+    let sizeGap = (currRect.width - minimalRect.width) / rectsCount;
+    for (let i = 0; i < rectsCount; i++){
+        drawRectangle(currRect);
+        currRect.width -= sizeGap;
+        currRect.height -= sizeGap;
+        currRect.x += sizeGap/2;
+        currRect.y += sizeGap/2;
+        currRect.color = colors[i+1];
+    }
 }
 
 function drawLine(){
