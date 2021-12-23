1ddf6ba36ab47ad5fcd2dd078ad4a615f0e304b2
leeorgo <84677813+leeorgo@users.noreply.github.com>
2021-12-23 20:51:27 +0200

    1

---
+++ Ex/2021-12-23/js/draw_shapes.js
@@ -0,0 +1,69 @@
+let canvas;
+let ctx;
+let colors = ["#C418E8", "#E84B18", "#18E823"];
+let colorIndex = 0;
+
+function initPage(){
+    console.log("initPage");
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    canvas.addEventListener("click", (event)=>{
+        let x = event.clientX - canvas.offsetLeft;
+        let y = event.clientY - canvas.offsetTop;
+        let currentColor = colors[colorIndex];
+        ctx.strokeStyle = currentColor;
+        colorIndex += 1;
+        if (colorIndex >= colors.length)
+            colorIndex = 0;
+        ctx.strokeRect(x, y, 10, 10); 
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+        ctx.lineTo(x + 30,y);
+        ctx.stroke();   
+        
+
+    });
+}
+
+function calcCoordinates(){
+    let colors=[];
+    let lineCount=3;
+    let lineCount=3;
+    let lineIndex=0;
+
+    let x = clientX - canvas.offsetLeft;
+    let y = clientY - canvas.offsetTop;
+    colorIndex += 1;
+    if (x >= colors.length)
+        colorIndex = 0;
+}
+
+function fillBackground(){
+    ctx.fillStyle = "orange";
+    //ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'blue';
+    ctx.strokeRect(10, 10, canvas.width-20, canvas.height-20);
+
+    //radial gradiant
+    var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 150);
+    grd.addColorStop(0, "orange");
+    grd.addColorStop(1, "red");
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);  
+}
+
+function drawLine(){
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'blue';
+    ctx.moveTo(100,50);
+    ctx.lineTo(200,100);
+    ctx.stroke();    
+}
+
+function drawCircle(){
+    ctx.strokeStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(95, 50, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+}
