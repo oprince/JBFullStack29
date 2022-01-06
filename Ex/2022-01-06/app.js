55d74ba67f62ab1f03b6176c8024751b32e71edb
Tamir Assayag <tamirassayag@gmail.com>
2022-01-06 22:02:36 +0200

    post request on add student

--- Ex/2022-01-06/app.js
+++ Ex/2022-01-06/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
 //setting middleware
 let staticFolder = path.join(__dirname, "public");
 app.use(express.static(staticFolder)); //Serves resources from public folder
+app.use(bodyParser()); //parses the body of the request
 
 //setting root API
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/student", (req, res) => {
+  const { data } = req.body;
+  console.log(data);
+  res.send("Student Added");
 });
 
 app.listen(port, () => {
