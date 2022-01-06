bde8be5e2ed9a9bfa53e2e980721bb6ee9e4a933
haim1212 <74496074+haim1212@users.noreply.github.com>
2022-01-06 20:37:04 +0200

    change
    change

---
+++ Ex/2022-01-06/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 3000
+
+//setting middleware
+let staticFolder = path.join(__dirname, 'public');
+app.use(express.static(staticFolder)); //Serves resources from public folder
+
+//setting root API
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/student', (req, res) => {
+  console.log("student was posted");
+  res.send("student was posted")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port} serving static content from folder ${staticFolder}`)
+});
