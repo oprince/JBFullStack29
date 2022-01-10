af9fbb52c0f364aa836b197a73d5bbd822162805
Tamir Assayag <tamirassayag@gmail.com>
2022-01-10 20:02:48 +0200

    comments

--- Ex/2022-01-10/Promises/js/promise_sample.js
+++ Ex/2022-01-10/Promises/js/promise_sample.js
@@ -3,9 +3,9 @@ let success = false;
 function startPromise() {
   console.log("startPromise");
   let promise = new Promise((resolve, reject) => {
-    success = !success;
+    success = !success; // toggling a boolean value
     setTimeout(() => {
-      if (success) {
+      if (success) { // checking if success then resolve else reject the promise
         resolve("Promise Success!!!");
       } else {
         reject("Promise Fail!!!");
@@ -16,11 +16,11 @@ function startPromise() {
     .then((message) => {
       console.log("Promise.then");
       $("#myDiv").html(message);
-      $("body").css("background-color", "green");
+      $("body").css({ "background-color": "green", color: "white" });
     })
     .catch((error) => {
       console.log("Promise.catch");
       $("#myDiv").html(error);
-      $("body").css("background-color", "red");
+      $("body").css({ "background-color": "red", color: "white" });
     });
 }
