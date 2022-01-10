36912818aad338ea722790f49e0451920e66ddc5
Tamir Assayag <tamirassayag@gmail.com>
2022-01-10 18:37:34 +0200

    refactor & getting forecast for the next 10 days.

---
+++ Ex/2022-01-10/jQuery AJAX API Call Example/app.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  $("#reset").click(function (e) {
+    $("#citySelect").val("Select");
+    $("#message").html("");
+  });
+
+  $("#submit").click(function (e) {
+    let validate = Validate();
+    $("#message").html(validate);
+    if (validate.length == 0) {
+      let selectedCity = $("#citySelect").val();
+
+      let BASE_URL = "http://api.openweathermap.org/data/2.5";
+      let API_KEY = "de6d52c2ebb7b1398526329875a49c57";
+      let API_URL = `${BASE_URL}/weather?id=${selectedCity}&appid=${API_KEY}&units=metric`;
+      let API_URL_FORECAST = `${BASE_URL}/forecast?id=${selectedCity}&cnt=10&appid=${API_KEY}&units=metric`;
+
+      $.get(API_URL_FORECAST, (data) => {
+        console.log({ forecast: data });
+      }).catch((xhr, status, error) => {
+        alert(
+          `Error: ${error} Status: ${status} Response: ${xhr.responseText}`
+        );
+      });
+
+      $.post(API_URL, (data) => {
+        drawCityWeather(data);
+      }).catch((xhr, status, error) => {
+        alert(
+          `Error: ${error} Status: ${status} Response: ${xhr.responseText}`
+        );
+      });
+    }
+  });
+
+  function drawCityWeather(result) {
+    var table = $("<table><tr><th>Weather Description</th></tr>");
+
+    table.append("<tr><td>City:</td><td>" + result["name"] + "</td></tr>");
+    table.append(
+      "<tr><td>Country:</td><td>" + result["sys"]["country"] + "</td></tr>"
+    );
+    table.append(
+      "<tr><td>Current Temperature:</td><td>" +
+        result["main"]["temp"] +
+        "°C</td></tr>"
+    );
+    table.append(
+      "<tr><td>Humidity:</td><td>" + result["main"]["humidity"] + "</td></tr>"
+    );
+    table.append(
+      "<tr><td>Weather:</td><td>" +
+        result["weather"][0]["description"] +
+        "</td></tr>"
+    );
+
+    $("#message").html(table);
+  }
+
+  $(document).ajaxStart(function () {
+    $("img").show();
+  });
+
+  $(document).ajaxStop(function () {
+    $("img").hide();
+  });
+
+  function Validate() {
+    var errorMessage = "";
+    if ($("#citySelect").val() == "Select") {
+      errorMessage += "► Select City";
+    }
+    return errorMessage;
+  }
+});
