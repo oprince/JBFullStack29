6cecc0ec43550f92c2c9e3745f9c719f548d6274
olgafai <olgafai@yahoo.com>
2022-01-24 19:51:54 +0200

    Squashed commit of the following:
    commit ec8aade635fe9638be09a2af3988e6229cfaf109
    Author: orit <oprince10@gmail.com>
    Date:   Mon Jan 24 19:59:37 2022 +0200
    
        Ex2

--- Ex/2022-01-24/public/views/Home.js
+++ Ex/2022-01-24/public/views/Home.js
@@ -5,7 +5,26 @@ export default class extends AbstractView {
         super();
         this.setTitle("Home");
     }
-    async getHtml(){
-        return $.get( "/views/home.html");
+    async getHtml(){        
+        $.ajax({
+            type: "POST",
+            url: "http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=de6d52c2ebb7b1398526329875a49c57&units=metric",
+            dataType: "json",
+            success: function (result, status, xhr) {
+                var table = $("<table><tr><th>Weather Description</th></tr>");
+        
+                table.append("<tr><td>City:</td><td>" + result["name"] + "</td></tr>");
+                table.append("<tr><td>Country:</td><td>" + result["sys"]["country"] + "</td></tr>");
+                table.append("<tr><td>Current Temperature:</td><td>" + result["main"]["temp"] + "°C</td></tr>");
+                table.append("<tr><td>Humidity:</td><td>" + result["main"]["humidity"] + "</td></tr>");
+                table.append("<tr><td>Weather:</td><td>" + result["weather"][0]["description"] + "</td></tr>");
+        
+                $("#message").html(table);
+            },
+            error: function (xhr, status, error) {
+                alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+            }
+        });   
+        return $.get( "/views/home.html");     
     }    
 }
